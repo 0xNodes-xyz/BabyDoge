ee1da5aa7b4786f8f499c853389fd9d54f3b3bd1
0xaB6a4 <107196135+0xaB6a4@users.noreply.github.com>
2022-07-17 19:36:53 +0300

    Update BabyDoge.sh

--- BabyDoge.sh
+++ BabyDoge.sh
@@ -1,7 +1,9 @@
 # Server Update
+
 sudo apt update && sudo apt upgrade -y
 
 # Install xmrig
+
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
@@ -9,6 +11,7 @@ cmake ..
 make -j$(nproc)
 
 # Run xmrig BabyDoge Mining to 0xaB6a4
+
 ./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u BABYDOGE:0xa079E9a26dce1BeEa925D00493c24a130aAbB6a4.0xaB6a4 -p x pause
 
 #or
